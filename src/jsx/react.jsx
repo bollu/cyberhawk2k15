dc82d35322081dfa21d7d2ccd11c4c072bed324e
bollu <siddu.druid@gmail.com>
2015-09-25 18:05:07 +0530

    get login page to some acceptable shape

--- src/jsx/react.jsx
+++ src/jsx/react.jsx
@@ -1,71 +1,99 @@
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
-var data = [
-{author: "Pete Hunt", text: "This is one comment"},
-{author: "Jordan Walke", text: "This is *another* comment"}
-];
-
-
-var Comment = React.createClass({
-    render: function() {
-        return (<div>author: {this.props.author} </div>);
-    }
-});
-
-var CommentList = React.createClass({
-    render: function() {
-        var commentNodes = this.props.data.map(function (comment) {
-            return (
-                    <Comment author={comment.author}>
-                    </Comment>
-                    );
-        });
-
-        return (
-                <div className="commentList">
-                {commentNodes}
-                </div>
-                );
-    }
-});
-
-var CommentBox = React.createClass({
+var InputContainer = React.createClass({
     getInitialState: function() {
-        return {data: []};
+        return {selected: false};
+    },
+    onFocus: function() {
+        console.log("focused");
+        this.setState({selected: true});
     },
 
-    loadCommentsFromServer: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                this.setState({data: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
-
-    componentDidMount: function() {
-        this.loadCommentsFromServer();
-        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    onBlur: function() {
+        console.log("blurred");
+        this.setState({selected: false});
     },
 
     render: function() {
-        return (
-                <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList data={this.state.data} />
-                <CommentForm />
-                </div>
-                );
-    }
-});
+        var underline_style =  {
+          transitionDuration: "1s",
+          color: '#ffffff',
+          backgroundColor: '#ffffff',
+          width: this.state.selected ? "100%" : "0px",
+          height: "2px",
+      };
 
+      var input_style = {
+        transitionDuration: "0.3s",
+        color: "#efefef",
+        border: "none",
+        outline: "none",
+        fontWeight: "300",
+        fontSize: "1.3em",
+        backgroundColor: "rgba(255, 255, 255, 0.0)",
+        width: "100%",
+        paddingLeft: "0.5em",
+        borderLeft: this.state.selected ? "2px solid rgba(0, 0, 0, 0)" : "2px solid #bfbfbf",
+    };
 
-React.render(
-             <CommentBox url="comments.json" pollInterval={2000} />,
-             document.getElementById('content')
-             );
+    var desc_style = {
+        transitionDuration: "0.3s",
+        paddingLeft: "-2px",
+        color: this.state.selected ? "#ffffff" : "#bfbfbf",
+        letterSpacing: "0.15em",
+        fontWeight: 300,
+    };
+
+    var container_style = {
+        transitionDuration: "1s",
+        marginBottom: "1em",
+        marginTop: "1em",
+    };
+    return (<div class="input-container" style={container_style}>
+            <span style={desc_style}>{this.props.placeholder}</span>
+            <input onFocus={this.onFocus}
+                onBlur={this.onBlur}
+                style={input_style}
+                type={this.props.type}></input>
+            <div style={underline_style}></div>
+            </div>);
+}
+})
+
+var LoginButton = React.createClass({
+    getInitialState: function() {
+        return {loggingIn: false};
+    },
+
+    onClick: function() {
+        this.setState({loggingIn: true});
+    },
+
+    render: function() {
+        var button_style = {
+          transitionDuration: "1s",
+          border: "none",
+          marginTop: "1em",
+          outline: "none",
+          fontWeight: "300",
+          fontSize: "1.3em",
+          width: this.state.loggingIn ? "2em" : "100%",
+          marginLeft: this.state.loggingIn ? "50%" : 0,
+          paddingTop: "0.3em",
+          paddingBottom: "0.3em",
+          backgroundColor: "#EE6383",
+          color: "#ffffff",
+          borderRadius: 30,
+      }
+      var text = this.state.loggingIn ?  "o" : "Submit" ;
+      return (<button style={button_style} onClick={this.onClick}> {text}</button>)
+  }
+})
+
+React.render((<div>
+                <InputContainer type="text" placeholder="Username"/>
+                <InputContainer type="password" placeholder="Password"/>
+                <LoginButton/>
+            </div>), 
+             document.getElementById('react-input-container'));
 
